0812bdb7983cb0041acc48acc1d5ee5fa285d044
Samuel Malaj <malajsamuel287@gmail.com>
2026-03-28 18:46:12 +0000

    Refactor calculation logic and add new functions
    Refactor calculation functions to improve readability and efficiency. Added compress_MultiplyDivide and get_compressed_MultiplyDivide functions for handling multiplication and division.

--- Software/Calculate.py
+++ Software/Calculate.py
@@ -1,7 +1,5 @@
 import math
-import operator
 import time
-import traceback
 
 def is_float(char): # checks if the value is a number
     try:
@@ -16,16 +14,46 @@ def join_nums(text): # joins all the side by side digits into one number for cal
     if len(text) > 1:
         while not end:
             counter += 1
-            for char in text[:-1]:
-                pos = text.index(char)
-
+            for pos, char in enumerate(text[:-1]):
                 if is_float(char) and is_float(text[pos + 1]): # checks if the value and the next value are both numbers
                     text[pos: pos + 2] = [text[pos] + text[pos + 1]]
+                    break
+                if char == '.':
+                    text[pos-1: pos + 2] = [text[pos-1] + '.' + text[pos + 1]]
                     break
 
                 if counter >= (len(text)-1):
                     end = True
     return text
+
+def compress_MultiplyDivide(expression): # does multipliplication + division
+    print(expression)
+    expression = join_nums(expression)
+    print('Joined: ',expression)
+    for char in expression:
+        if char == 'x':
+            try:
+                num1 = float(expression[expression.index(char)-1])
+                num2 = float(expression[expression.index(char)+1])
+                compressed_value = num1 * num2
+                expression[expression.index(char)-1: expression.index(char)+2] = str(compressed_value)
+
+                return expression
+
+            except:
+                return ['Syntax Error']
+
+        if char == '/':
+            try:
+                num1 = float(expression[expression.index(char)-1])
+                num2 = float(expression[expression.index(char)+1])
+                compressed_value = num1 / num2
+                expression[expression.index(char)-1: expression.index(char)+2] = str(compressed_value)
+
+                return expression
+
+            except:
+                return ['Syntax Error']
 
 
 def compress_AddSub(expression): # function does addition and subtraction
@@ -56,14 +84,24 @@ def compress_AddSub(expression): # function does addition and subtraction
             except:
                 return ['syntax error']
 
+def get_compressed_MultiplyDivide(expression): # keeps multiplying and dividing untill all multiplication + division is done
+    while 'x' in expression or '/' in expression:
+        expression = compress_MultiplyDivide(expression)
+    return expression 
+
 def get_compressed_AddSub(expression): # function keeps going through expression untill all addition and subtraction is done
     while '+' in expression or ('-' in expression and '-' not in expression[0]):
         expression = compress_AddSub(expression)
-        print(expression)
-
-    print('FINAL: ', expression)
     return expression
 
-expression = ['5', '6', '+', '3', '-', '1', '0', '-', '1', '0', '0', '+']
-answer = get_compressed_AddSub(expression)
+def calculate(expression):
+    expression = get_compressed_MultiplyDivide(expression)
+    print(expression)
+    expression = get_compressed_AddSub(expression)
+
+    return expression
+
+
+expression = ['2', '+', '5', 'x', '5', '2', '+', '2', '6', 'x', '2']
+answer = calculate(expression)
 print(answer)
