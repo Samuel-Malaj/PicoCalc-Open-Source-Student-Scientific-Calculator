11bcb93ffe502004b981505875b28099ff440f6f
Samuel Malaj <malajsamuel287@gmail.com>
2026-05-23 17:18:21 +0100

    Refactor OLED display functions and add clear_main
    Added functions to clear the main display area and updated text display positions.

--- Software/OLED.py
+++ Software/OLED.py
@@ -5,13 +5,17 @@ import time
 i2c = I2C(0, sda=Pin(16), scl=Pin(17), freq=400_000)
 oled = SSD1309(sda=16, scl=17, addr=0x3D)
 
-def clear():
+def clear_all():
     oled.clear()
+    oled.show()
+    
+def clear_main():
+    oled.rect(0, 10, 128, 54, 0, fill=True)
     oled.show()
     
 def display_input(text):
     oled.clear()
-    oled.text(text, 0, 0)
+    oled.text(text, 0, 10)
     oled.show()
     
 def display_output(text):
@@ -27,9 +31,8 @@ def display_line(text, x, y):
     oled.clear()
     oled.text(text, x, y)
     oled.show()
-    
-display_output('hello')
 
+#display_output('hello')
 # while True:
 #     print([hex(d) for d in i2c.scan()])
 #     time.sleep(1)
@@ -38,3 +41,5 @@ display_output('hello')
 #         display_output('hello')
 #     except Exception as e:
 #         print(e)
+
+
