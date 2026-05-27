d9016719224e0c9ff14cee60b86a7ba3f9be5c1b
Samuel Malaj <malajsamuel287@gmail.com>
2026-05-27 16:40:29 +0100

    Refactor OLED display functions and add clear_mode_line

--- Software/OLED.py
+++ Software/OLED.py
@@ -5,6 +5,10 @@ import time
 i2c = I2C(0, sda=Pin(16), scl=Pin(17), freq=400_000)
 oled = SSD1309(sda=16, scl=17, addr=0x3D)
 
+def clear_mode_line():
+    oled.rect(0, 0, 106, 10, 0, fill=True)
+    oled.show()
+
 def clear_all():
     oled.clear()
     oled.show()
@@ -41,5 +45,3 @@ def display_line(text, x, y):
 #         display_output('hello')
 #     except Exception as e:
 #         print(e)
-
-
