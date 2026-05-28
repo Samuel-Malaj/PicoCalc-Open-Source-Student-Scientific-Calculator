d560445aeeb02891511b4a77db1d5bcb3ef19b3a
Samuel Malaj <malajsamuel287@gmail.com>
2026-05-28 09:10:46 +0100

    Remove accidental change to modes.py and add new note_pad function.
    New note_pad function allows user to write and save a note to a text file.

--- Software/modes.py
+++ Software/modes.py
@@ -1,81 +1,124 @@
-from machine import Pin
-import time
-import network
-import errno
-from icons import *
+import uasyncio as asyncio
+from Calculate import calculate
 from inputs import *
+import time
+from OLED import *
 from WiFi import *
-from modes import *
-from utils import *
+import urequests
 
-LED = Pin('LED', Pin.OUT)
-while True:
-    try:
-        from OLED import *
-        break
-    except Exception as e:
-        print(e)
-        LED.toggle()
-        time.sleep(0.5)
-        LED.toggle()
-# --- 1. SETUP ---
-# Safety: If GP1 is connected to GND, don't sleep!
-
-##############################################################################
-#print('Switching off')
-#power_off()
-
-        
- 
-''' Main '''
-def main():
-    prepare_socket()
+''' Calculator '''
+def calculator(expression):
+    ANS = '0'
+    clear_main()
     while True:
-        try:
-            indicate_wifi()
-            clear_mode_line()
+        function, expression = get_expression(calculator_array, character_array, ANS, line=10)
+        if function == 'EXE':
+            answer = calculate(expression)
+            if 'Error' not in answer:
+                ANS = answer         
+            print(answer)
             clear_main()
-            append_output('Calc Mode', 0, 0)
-            calculator(expression)
-                
-            indicate_wifi()
-            clear_mode_line()
-            clear_main()
-            append_output('Notepad', 0, 0)
-            note_pad(expression)
-                    
-            ## messaging
-            indicate_wifi()
-            clear_main()
-            clear_mode_line()
-            append_output('SMS Mode', 0, 0)
-            if ap.active() or wlan.isconnected():
-                asyncio.run(messaging())
-            else:
-                append_output('Unavailable', 0, 10)
-                append_output('No Wi-Fi', 0, 20)
-                time.sleep(1.5)
-            
-            ## whatsapp
-            indicate_wifi()
-            clear_mode_line()
-            clear_main()
-            append_output('Whatsapp', 0, 0)
-            if wlan.isconnected():
-                send_whatsapp()
-            else:
-                append_output('Unavailable', 0, 10)
-                append_output('No WiFi', 0, 20)
-                time.sleep(1.5)
+            append_output(answer, 0, 54)
+        if function == 'WiFi':
+            wifi()
+        if function == 'MODE':
+            return ''
+
+''' Characters '''        
+def note_pad(expression):
+    while True:
+        open('notes.txt', 'a').close()
+        with open('notes.txt', 'r') as file:
+            note = file.readlines()
         
-        except Exception as e:
-            print(e)
+        clear_main()
+        append_output('Note:' + ''.join(note), 0, 10)
+        function, expression = get_expression(character_array, shift_character_array, 0, line=20)
+        
+        if function == 'EXE':
+            with open('notes.txt', 'w') as file:
+                file.write(''.join(expression))
+                file.close()
+            clear_main()
+            append_output('Saved Note', 0, 10)
+    
+        if function == 'WiFi':
+            wifi()
+        if function == 'MODE':
+            return ''
+
+''' SMS '''
+stop_event = asyncio.Event()
+async def recv_task():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setblocking(False)
+    s.bind(('', 5005))
+    try:
+        while not stop_event.is_set():
             try:
-                display_line('System Error', 0, 10)
-            except:
-                LED.toggle()
-                time.sleep(1)
-                LED.toggle()
-                
-main()
+                data, addr = s.recvfrom(1024)
+                string = data.decode()
+                clear_main()
+                append_output('Received:', 0, 44)
+                append_output(string, 0, 54)
+            except OSError:
+                pass
+            await asyncio.sleep(0.01)
+    finally:
+        s.close()
+
+async def send_task(broadcast_ip):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setblocking(False)
+    try:
+        while not stop_event.is_set():
+            oled.rect(0, 10, 128, 20, 0, fill=True)
+            oled.show()
+            function, expression = get_expression(character_array, shift_character_array, 0, line=20)
+            if function == 'MODE':
+                print("Mode change detected. Stopping...")
+                stop_event.set()
+            elif expression:
+                msg = "".join(expression)
+                try:
+                    s.sendto(msg.encode(), (broadcast_ip, 5005))
+                    print(f"Sent: {msg}")
+                    append_output('Sent:', 0, 10)
+                    append_output(msg, 0, 20)
+                    await asyncio.sleep(1)  # not time.sleep
+                    clear_main()
+                except OSError as e:
+                    print(e)
+            await asyncio.sleep(0.1)
+    finally:
+        s.close()
+
+async def messaging():
+    stop_event.clear()  # reset for reuse
+    if ap.active():
+        ip_info = ap.ifconfig()
+    if wlan.active():
+        ip_info = wlan.ifconfig()
+    broadcast_ip = ".".join(ip_info[0].split('.')[:-1]) + ".255"
+    print('Messaging Mode')
+    await asyncio.gather(recv_task(), send_task(broadcast_ip))
+    
+''' Whatsapp ''' 
+def send_whatsapp():
+    while True:
+        function, expression = get_expression(character_array, shift_character_array, 0, line=20)
+        if function == 'MODE':
+            break
+        msg = '+'.join(''.join(expression).split(' '))
+        print('Sending:', msg)
+        append_output('Sending:', 0, 10)
+        append_output(msg, 0, 20)
+        api_key = '5801456'
+        phone_number = '+447309045098'
+        url = f"https://api.callmebot.com/whatsapp.php?phone={phone_number}&text={msg}&apikey={api_key}"
+        print(url)
+        response = urequests.get(url)
+        print(response.text)
+        response.close()
+        clear_main()
 
