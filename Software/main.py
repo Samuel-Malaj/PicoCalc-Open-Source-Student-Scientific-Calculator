387c9293c59a07a61229eabf38edc0e297c0ac20
Samuel Malaj <malajsamuel287@gmail.com>
2026-05-28 16:47:44 +0100

    Feat: implemented new main_menu function
    Main_menu function displays different icons for different modes and allows user to scroll through them and select a mode.

--- Software/main.py
+++ Software/main.py
@@ -30,23 +30,20 @@ while True:
 ''' Main '''
 def main():
     while True:
-        try:
-            indicate_wifi()
-            clear_mode_line()
-            clear_main()
+        mode = main_menu()
+        indicate_wifi()
+        clear_mode_line()
+        clear_main()
+        
+        if mode == 'calc mode':
             append_output('Calc Mode', 0, 0)
             calculator(expression)
-                
-            indicate_wifi()
-            clear_mode_line()
-            clear_main()
+            
+        if mode == 'notepad mode':
             append_output('Notepad', 0, 0)
             note_pad(expression)
-                    
-            ## messaging
-            indicate_wifi()
-            clear_main()
-            clear_mode_line()
+            
+        if mode == 'sms mode':
             append_output('SMS Mode', 0, 0)
             if ap.active() or wlan.isconnected():
                 asyncio.run(messaging())
@@ -54,26 +51,5 @@ def main():
                 append_output('Unavailable', 0, 10)
                 append_output('No Wi-Fi', 0, 20)
                 time.sleep(1.5)
-            
-            ## whatsapp
-            indicate_wifi()
-            clear_mode_line()
-            clear_main()
-            append_output('Whatsapp', 0, 0)
-            if wlan.isconnected():
-                send_whatsapp()
-            else:
-                append_output('Unavailable', 0, 10)
-                append_output('No WiFi', 0, 20)
-                time.sleep(1.5)
-        
-        except Exception as e:
-            print(e)
-            try:
-                display_line('System Error', 0, 10)
-            except:
-                LED.toggle()
-                time.sleep(1)
-                LED.toggle()
                 
 main()
